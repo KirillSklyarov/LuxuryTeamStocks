18c2a21ad09b7538e3cc717d693e5a6d818989fc
Kirill_Sklyarov <k.sklyarov@yandex.ru>
2025-07-19 12:05:57 +0300

    feat(app): - initial commit

---
+++ LuxuryTeamStocks/LuxuryTeamStocks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LuxuryTeamStocks
+//
+//  Created by Kirill Sklyarov on 19.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
